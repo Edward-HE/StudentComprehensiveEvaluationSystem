de5ce19eb0045d79687f2f43519e34f33d7f6266
Edward-HE <Edward-HE@users.noreply.github.com>
2019-05-22 00:25:30 +0800

    完成学生数据结构体

--- 学生综合评价系统/zhpjsystem.c
+++ 学生综合评价系统/zhpjsystem.c
@@ -1,3 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
-#
+#include<string.h>
+#include<conio.h>
+
+
+int saveflag = 0;
+
+typedef struct student {
+	int num;//学号
+	char name[15];//学生名字
+	char sex[4];//性别
+	char address[100];//家庭住址
+	char phone[14];//电话
+};
+
+typedef struct studata {
+	int num;//学号
+	int chinese, math, english, pinde;//语、数、英、思想品德成绩
+	int rank_main, rank_all;//语数英三科名次，综合测评名次
+	int huping, teacher;//互评分，教师评分
+	float average, score_all;//三科平均分，综合总分
+};
+
